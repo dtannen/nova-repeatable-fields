22d7962d5bdbad22781fef6efa81eee0ace9a865
dtannen <dtannen@gmail.com>
2020-11-04 15:15:58 -0500

    add step to number field

--- resources/js/components/sub-fields/NumberSubField.vue
+++ resources/js/components/sub-fields/NumberSubField.vue
@@ -4,6 +4,7 @@
             :id="subField.name"
             :name="subField.name"
             type="number"
+            step=".01"
             class="w-full form-control form-input form-input-bordered"
             :value="value"
             v-bind="subField.attributes"
